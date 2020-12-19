959f123aa6e2168752abbad2253f89383d46fda2
Waqas Noor <waqasn2c@gmail.com>
2020-12-19 17:37:40 +0500

    initial commit

---
+++ src/sequelize.ts
@@ -0,0 +1,21 @@
+import { Sequelize } from "sequelize-typescript";
+import { config } from "./config/config";
+
+export const sequelize = new Sequelize({
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
+
+  dialect: config.dialect,
+  storage: ":memory:",
+});
+
+console.log({
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
+
+  dialect: config.dialect,
+});
