9edba908f23197b8f7e84d5be1f4b489abe7a447
Waqas Noor <waqasn2c@gmail.com>
2020-12-21 01:38:45 +0500

    feat: cors support allow * origin

--- src/server.ts
+++ src/server.ts
@@ -34,8 +34,18 @@ import { IndexRouter } from "./controllers/v0/index.router";
   //     origin: config.url,
   //   })
   // );
+
   app.use((req, _, next) => {
     console.log(`${req.method} ${req.originalUrl}`);
+    next();
+  });
+
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
     next();
   });
 
